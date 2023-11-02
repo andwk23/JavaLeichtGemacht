a73bdd7d08aa9ea54c99094aba089fae0a2e0930
andwk23 <fuerschrott@online.de>
2023-11-02 19:04:30 +0100

    Java Vorstellung der Klasse String - noch keine Methoden

---
+++ src/_01_Grundlagen/Zeichenketten_Strings.java
@@ -0,0 +1,20 @@
+package _01_Grundlagen;
+
+public class Zeichenketten_Strings {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n*** Die Klasse String ***");
+		System.out.println("-------------------------\n");
+		
+		String stringBeispiel = "Ich bin eine Zeichenkette";
+		String name = "Wiwi";
+		String age = "68";
+		char ausrufezeichen = '!';
+		
+		System.out.println("\tBeispielzeichenkette:\n\t" + stringBeispiel);
+		System.out.println("\n\tZusammengesetzte Zeichenkette:\n\t" + name + " " + age + " " + ausrufezeichen);
+
+	}
+
+}
