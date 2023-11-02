8803e563b63947ac4c7a0cf7294d9909e36f9300
andwk23 <fuerschrott@online.de>
2023-11-02 18:38:26 +0100

    Java 3 Arten von Kommentare

---
+++ src/_01_Grundlagen/Kommentare.java
@@ -0,0 +1,30 @@
+package _01_Grundlagen;
+
+/**
+ * 
+ * Kommentare bei Java
+ * Dokumentationskommentar
+ * 
+ */
+
+public class Kommentare {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n*** Es gibt in Java 3 Arten von Kommentaren ***");
+		System.out.println("-----------------------------------------------\n");
+		System.out.println("--- Dokumentationskommentar: /** * * */ ---");
+		System.out.println("\n--- Zeilenkommentar: // ---");
+		System.out.println("\n--- Blockkommentarkommentar: /* * * */ ---");
+		
+		// Zeilenkommentar
+		
+		/*
+		 * 
+		 * Blockkommentar
+		 * 
+		 */
+
+	}
+
+}
